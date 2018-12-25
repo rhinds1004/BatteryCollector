72849f67403ec963e3ad8efb02037147ab090682
rhinds1004 <rhinds1004@gmail.com>
2018-12-24 18:26:31 -0800

    added logic to slowly remove power for the character over time. NOTE: GameModeBase does not have tick function enabled by default have to manual start the tick. This is different

--- Source/BatteryCollector/BatteryCollectorGameMode.cpp
+++ Source/BatteryCollector/BatteryCollectorGameMode.cpp
@@ -3,6 +3,7 @@
 #include "BatteryCollectorGameMode.h"
 #include "BatteryCollectorCharacter.h"
 #include "UObject/ConstructorHelpers.h"
+#include "Kismet/GameplayStatics.h"
 
 ABatteryCollectorGameMode::ABatteryCollectorGameMode()
 {
@@ -11,5 +12,26 @@ ABatteryCollectorGameMode::ABatteryCollectorGameMode()
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
+		PrimaryActorTick.bCanEverTick = true; //tick is set to false in gamemodebase
+	}
+
+	//base decay rate
+	DecayRate = 0.01f;
+}
+
+void ABatteryCollectorGameMode::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	//Check using a battery collector character
+	ABatteryCollectorCharacter* MyCharacter = Cast<ABatteryCollectorCharacter>(UGameplayStatics::GetPlayerPawn(this, 0));
+	if (MyCharacter)
+	{
+		//If power is positive
+		if (MyCharacter->GetCurrentPower())
+		{
+			//decrease the character's power using the decay rate
+			MyCharacter->UpdatePower(-DeltaTime*DecayRate*(MyCharacter->GetCurrentPower()));
+		}
 	}
 }
